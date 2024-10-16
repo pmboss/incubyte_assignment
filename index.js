83dd424088594a69a4a4e2c532725cac5c9ba283
Pradhumna <pradhumnamalakar@gmail.com>
2024-10-16 17:38:37 +0530

    feat: add delimeter functionality to string addition

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
-function calculateSum(){
+function calculateSum() {
     var input = document.getElementById('inputString').value;
     var output = document.getElementById('result');
     var error = document.getElementById('errorMessage');
 
-    if(!input){
+    if (!input) {
         output.innerText = 0;
         return;
     }
 
-    try{
+    try {
         const sum = add(input)
         output.innerText = sum;
         error.innerText = '';
         return;
 
-    }catch (e){
+    } catch (e) {
         error.innerText = e.message;
     }
 
-    function add(input){
-        const numbers =  input.split(',').map(Number);
-        if(numbers.some(isNaN)){
+    function add(input) {
+        let delimiter = /,|\\n/;
+        let negativeNo = [];
+        const numbers = input.split(delimiter).map(Number);
+        if (numbers.some(isNaN)) {
             throw new Error('Input string contains non-numeric values');
         }
-    
-        const total = numbers.reduce((a, b) => a + b, 0);
+
+        const total = numbers.reduce((a, b) => {
+            if (b < 0) {
+                negativeNo.push(b);
+            }
+            return a + b;
+        }, 0);
+
+        if (negativeNo.length) {
+            throw new Error(`negative numbers not allowed: ${negativeNo.join(', ')}`);
+        }
 
         return total;
     }
-   
+
 
 }
